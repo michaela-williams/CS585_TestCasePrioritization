7c1ab996fc37e1ba70b9744a91dafa046e1dba9d
michaela-williams <michaela.williams359@gmail.com>
2020-05-03 22:57:54 -0400

    Edit

--- TestPrioritize.py
+++ TestPrioritize.py
@@ -1,5 +1,6 @@
 import time
 from random import randrange
+from copy import deepcopy
 
 maxNumTests = 300000
 sortSize = 1000
@@ -57,6 +58,7 @@ def read_generated():
     with open('TestData.csv') as file:  
         read_data = file.readline()
         while not read_data == '':
+            failRatio = 0
             cur_test = list()
             for index in range(8):
                 extracted_data = read_data.partition(',')
@@ -64,8 +66,14 @@ def read_generated():
                     cur_test.append(int(extracted_data[0]))
                 elif index == 2 or index == 6:
                     cur_test.append(float(extracted_data[0]))
-                elif index == 3 or index == 4 or index == 5:
+                elif index == 3:
                     cur_test.append(int(extracted_data[0]))
+                elif index == 4:
+                    failRatio = float(extracted_data[0])
+                elif index == 5:
+                    print(extracted_data[0])
+                    failRatio = failRatio / float(extracted_data[0])
+                    cur_test.append(failRatio)
                 elif index == 7:
                     extracted_data = extracted_data[0].partition('\n')
                     cur_test.append(float(extracted_data[0]))
@@ -80,38 +88,24 @@ def read_generated():
     return tests
 
 #test order randomly selected
-def random(testData):
+def random(origData):
+    testData = deepcopy(origData)
     ordered = list()
     maxRand = len(testData)
     for cur in range(maxRand):
         curTestNum = randrange(0, len(testData))
         curTest = testData.pop(curTestNum)
-        ordered.append((curTest[0], curTest[1]))
+        ordered.append(curTest)
         
     elapsed = 0 #value to keep the return format the same as other sort functions
     
     return (ordered, elapsed)
 
-def simulateTests(testQueue):
-    duration = 2
-    passed = 1
-    testsRun = 0
-    timeRun = 0
-    print(testQueue)
-    for curTest in testQueue:
-        testsRun += 1
-        timeRun += curTest[duration]
-        if not curTest[passed]:
-            print(curTest)
-            break
-        
-    report(testsRun, timeRun)
-        
-
-def smallest_sort(testData):
+def smallest_sort(origData):
     startTime = time.clock()
     testQueue = list()
     index = 3
+    testData = deepcopy(origData)
     
     while len(testData) > 0:
         start_index = 0
@@ -138,9 +132,60 @@ def smallest_sort(testData):
     
     return (testQueue, elapsed)
 
+#sort by highest fail ratio
+def failRatio_sort(origData):
+    startTime = time.clock()
+    testQueue = list()
+    index = 4
+    testData = deepcopy(origData)
+    
+    while len(testData) > 0:
+        start_index = 0
+        end_index = sortSize
+        while start_index < len(testData) - 1:
+            if len(testData) == 1:
+                testQueue.append(testData[0])
+                break
+        if (end_index >= len(testData)):
+            end_index = len(testData) - 1
+        curLargest = findMax(testData, index, start_index, end_index)
+        testQueue.append(testData[curLargest])
+        testData.pop(curLargest)
+        start_index = end_index
+        end_index += sortSize
+        
+        if (len(testData) == 1):
+            testQueue.append(testData[0])
+            testData.pop()
+            break
+        
+    endTime = time.clock()
+    elapsed = endTime - startTime
+                            
+     return (testQueue, elapsed)                        
+
+def simulateTests(testQueue):
+    duration = 2
+    passed = 1
+    testsRun = 0
+    timeRun = 0
+    for curTest in testQueue:
+        testsRun += 1
+        timeRun += curTest[duration]
+        if not curTest[passed]:
+            break
+        
+    report(testsRun, timeRun)
+
 def report(testsRun, timeRun):
-    timeInMin = timeRun / 60
-    print("Ran ", testsRun, "tests for ", timeInMin, " minutes before failing.")
+    units = "seconds"
+    if timeRun > 60:
+        timeRun = timeRun / 60
+        units = "minutes"
+    if timeRun > 60:
+        timeRun = timeRun / 60
+        units = "hours"
+    print("Ran ", testsRun, "tests for ", timeRun, " ", units, " before failing.")
 
 def report_sort(timeToSort, name):
     units = "seconds"
@@ -161,11 +206,34 @@ def findMin(data, index, start_index, end_index):
         
     return curMinIndex
 
-#testing
-cur_tests = read_testShare()
-#cur_tests = read_generated()
+def findMax(data, index, start_index, end_index):
+    curMax = -1
+    curMaxIndex = -1
+    curIndex = start_index
+    while cur_index < end_index:
+        if data[curIndex][index] < curMax:
+            curMax = data[curIndex][index]
+            curMaxIndex = curIndex
+        curIndex += 1
+        
+    return curMaxIndex
 
-smallest = smallest_sort(cur_tests)
-report_sort(smallest, "smallest_sort")
-simulated = simulateTests(smallest[0])
+#testing
+#cur_tests = read_testShare()
+cur_tests = read_generated()
+print(cur_tests)
+
+#simulateTests(cur_tests)
+
+'''
+rand = random(cur_tests)
+simulateTests(rand[0])
+rand = random(cur_tests)
+simulateTests(rand[0])
+rand = random(cur_tests)
+simulateTests(rand[0])
+'''
+#smallest = smallest_sort(cur_tests)
+#report_sort(smallest[1], "smallest_sort")
+#simulateTests(smallest[0])
 
